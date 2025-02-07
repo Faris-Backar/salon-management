60bfce80a09e7dab2e3e9b46080bc3997b999bf5
faris-backer <faris.backer@lxisoft.com>
2025-02-07 19:02:30 +0530

    Basic Folder structure and Configuration Completed.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:salon_management/main.dart';
+import 'package:salon_management/salon_management_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SalonManagementApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
